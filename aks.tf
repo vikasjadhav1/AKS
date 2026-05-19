57120d1b7710b033c64a20b91dd73ea7380c01cc
Vikas Jadhav <76843784+vikasjadhav1@users.noreply.github.com>
2026-05-19 18:33:05 +0530

    update 1

--- aks.tf
+++ aks.tf
@@ -5,7 +5,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = var.location
 
   resource_group_name = azurerm_resource_group.rg.name
-
+  oidc_issuer_enabled = true
   dns_prefix = var.dns_prefix
 
   default_node_pool {
